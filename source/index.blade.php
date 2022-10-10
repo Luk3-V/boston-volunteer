76977f30eaa8e4225e30909ba370f577c005f0c2
Luk3-V <38675245+Luk3-V@users.noreply.github.com>
2022-10-10 15:05:51 -0500

    Add form test

--- source/index.blade.php
+++ source/index.blade.php
@@ -88,6 +88,19 @@
 			</aside>
 		</div>
 	</section>
+	<section>
+		<form name="contact" method="POST" data-netlify="true">
+			<p>
+				<label>Your Name: <input type="text" name="name" /></label>
+			</p>
+			<p>
+				<label>Your Email: <input type="email" name="email" /></label>
+			</p>
+			<p>
+				<button type="submit">Send</button>
+			</p>
+		</form>
+	</section>
 
 <!--
 	TODO: Include a credits page.
